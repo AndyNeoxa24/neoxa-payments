7301f9028377666377022a2da92a8518b0460350
Andy Niemand <andy@neoxa.net>
2024-11-23 15:24:08 +0200

    Enhance RPC functionality with additional Neoxa wallet commands

--- includes/class-neoxa-rpc.php
+++ includes/class-neoxa-rpc.php
@@ -69,14 +69,76 @@ class Neoxa_RPC {
         $balances = $this->request('listassetbalancesbyaddress', array($address));
         return isset($balances[$asset_name]) ? $balances[$asset_name] : 0;
     }
-    
+
+    public function get_address_deltas($address) {
+        return $this->request('getaddressdeltas', array(array("addresses" => array($address))));
+    }
+
+    public function get_address_mempool($address) {
+        return $this->request('getaddressmempool', array(array("addresses" => array($address))));
+    }
+
+    public function get_address_utxos($address) {
+        return $this->request('getaddressutxos', array(array("addresses" => array($address))));
+    }
+
     public function get_asset_data($asset_name) {
         return $this->request('getassetdata', array($asset_name));
     }
 
+    public function list_addresses_by_asset($asset_name, $onlytotal = false) {
+        return $this->request('listaddressesbyasset', array($asset_name, $onlytotal));
+    }
+
+    public function get_asset_snapshot($asset_name, $block_height) {
+        return $this->request('getsnapshot', array($asset_name, $block_height));
+    }
+
+    public function get_block_count() {
+        return $this->request('getblockcount');
+    }
+
+    public function get_mempool_info() {
+        return $this->request('getmempoolinfo');
+    }
+
+    public function get_tx_out($txid, $n, $include_mempool = true) {
+        return $this->request('gettxout', array($txid, $n, $include_mempool));
+    }
+
+    public function verify_payment($address, $expected_amount, $asset = 'NEOXA') {
+        try {
+            $mempool_txs = $this->get_address_mempool($address);
+            $mempool_balance = 0;
+            foreach ($mempool_txs as $tx) {
+                if (isset($tx['satoshis'])) {
+                    $mempool_balance += $tx['satoshis'];
+                }
+            }
+
+            $balance = $this->get_address_balance($address);
+            $total_balance = ($balance['balance'] ?? 0) + $mempool_balance;
+
+            if ($asset === 'NEOXA') {
+                return $total_balance >= $expected_amount;
+            } else {
+                $asset_data = $this->get_asset_data($asset);
+                if (!$asset_data) {
+                    throw new Exception('Asset not found: ' . $asset);
+                }
+
+                $asset_balance = $this->get_asset_balance($address, $asset);
+                return $asset_balance >= $expected_amount;
+            }
+        } catch (Exception $e) {
+            error_log('Neoxa payment verification error: ' . $e->getMessage());
+            return false;
+        }
+    }
+
     public function test_connection() {
         try {
-            $this->request('getblockcount');
+            $this->get_block_count();
             return true;
         } catch (Exception $e) {
             return false;
@@ -86,7 +148,6 @@ class Neoxa_RPC {
     public function get_all_assets() {
         try {
             $assets = $this->list_assets("", true);
-            // Filter out restricted assets and qualifiers
             return array_filter($assets, function($asset) {
                 return !isset($asset['restricted']) && !isset($asset['qualifier']);
             });
@@ -103,15 +164,6 @@ class Neoxa_RPC {
         } catch (Exception $e) {
             error_log('Error validating Neoxa address: ' . $e->getMessage());
             return false;
-        }
-    }
-
-    public function get_transaction($txid) {
-        try {
-            return $this->request('getrawtransaction', array($txid, true));
-        } catch (Exception $e) {
-            error_log('Error getting Neoxa transaction: ' . $e->getMessage());
-            return null;
         }
     }
 }
